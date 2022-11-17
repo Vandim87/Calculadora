56de0e1e89a1216032bb590828a89a70988c6629
Vandim87 <107519365+Vandim87@users.noreply.github.com>
2022-11-17 16:43:41 -0300

    1

--- calculadora.js
+++ calculadora.js
@@ -1,19 +1,40 @@
-function criaCalculadora(){
-    return{
-        display: document.querySelector('.display'),
-        inicia(){
-            this.cliqueBotoes();
-        },
-        cliqueBotoes(){
-            document.addEventListener('click', function(e){
-                const el = e.target;
-                if(el.classList.contains('btn-num')){this.btnParaDisplay(el.innerText);}}.bind(this));
-            },
-        btnParaDisplay(valor) {
-            this.display.value += valor;
+function Calculadora(){
+ this.display = document.querySelector('.display');
+
+ this.inicia = () => {this.capturaCliques(); this.capturaEntre()};
+
+ this.capturaEntre = () => {
+    document.addEventListener('keyup', e=>{
+        if(e.keyCode !== 13) return;
+        this.realizaConta();
+    });
+ };
+
+ this.capturaCliques = () => {
+    document.addEventListener('click', e => {const el = e.target;
+     if(el.classList.contains('btn-num'))this.addNumDisplay(el);
+     if(el.classList.contains('btn-clear'))this.clear();
+     if(el.classList.contains('btn-del'))this.del();
+     if(el.classList.contains('btn-eq'))this.realizaConta();
+    });};
+
+    this.addNumDisplay = el => {this.display.value += el.innerText;
+    this.display.focus();};
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice (0,-1);
+    this.realizaConta = () => {
+        try{
+            const conta = eval(this.display.value);
+            if(!conta){alert('Conta inválida');return}
+        this.display.value = conta;
         }
+        catch(e){
+            alert('Conta inválida');
+            return;
         }
+    };
+
 }
 
-const calculadora = criaCalculadora();
+const calculadora = new Calculadora();
 calculadora.inicia();
