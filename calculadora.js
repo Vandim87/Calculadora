b0941179e6910f9559993a83b1441f015761795c
Vandim87 <107519365+Vandim87@users.noreply.github.com>
2022-11-16 23:55:38 -0300

    1

--- calculadora.js
+++ calculadora.js
@@ -2,12 +2,17 @@ function criaCalculadora(){
     return{
         display: document.querySelector('.display'),
         inicia(){
-            alert('oi, iniciar');
+            this.cliqueBotoes();
         },
         cliqueBotoes(){
-            
-        },
-    };
+            document.addEventListener('click', function(e){
+                const el = e.target;
+                if(el.classList.contains('btn-num')){this.btnParaDisplay(el.innerText);}}.bind(this));
+            },
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+        }
 }
 
 const calculadora = criaCalculadora();
